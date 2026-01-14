f2f18a257b89f66897a5e9d9a2823ee40e5d04e7
TamirisKashay <tajsyamirlan@gmail.com>
2026-01-14 21:48:28 +0500

    Add Main program logic

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Driver driver1 = new Driver("Alex", "DL12345");
+        Driver driver2 = new Driver("Maria", "DL67890");
+
+        Vehicle car = new Car("Toyota", 2020, driver1, 4, "Petrol");
+        Vehicle motorcycle = new Motorcycle("Yamaha", 2018, driver2, false);
+        Vehicle truck = new Truck("Volvo", 2015, driver1, 12.5, 6);
+
+        Vehicle[] vehicles = { car, motorcycle, truck };
+
+        for (Vehicle v : vehicles) {
+            v.startEngine();
+            v.displayInfo();
+            v.stopEngine();
+            System.out.println("----------------------");
+        }
+    }
+}
 
